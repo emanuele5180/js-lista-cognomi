8e149e52764bc4b2e0bb12d2e540dcb1f084dedf
emanuele5180 <macina5180>
2020-06-24 15:56:47 +0200

    aggiungi

---
+++ main.js
@@ -0,0 +1,10 @@
+//Per l'esercizio di oggi, chiedete all'utente un cognome e aggiungetelo alla lista, poi ordinate, utilizzate una funzionalità di js per l'ordinamento.
+//Dopo aver ordinato, l'esercizio chiede l'indice del nuovo elemento inserito, per farlo utilizzate il metodo della ricerca dell'ultima esercitazione.
+
+
+var cognome = prompt( "aggiungi il cognome");
+
+var lista = ["Pippo", "Topolino", "Paperino","Gastone"];
+
+lista.push(cognome);
+console.log(lista);
